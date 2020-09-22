dba00d8d5dc9387740e67666fac6e9fa63adb4d4
Sven Fischer <git-dev@linux4tw.de>
2020-09-22 22:39:01 +0200

    zero-fill munich suburb number

--- munich_suburbs.py
+++ munich_suburbs.py
@@ -3,6 +3,7 @@ import datetime
 import networkx as nx
 import os
 import osmnx as ox
+import re
 import sys
 
 from run import OsmCalc
@@ -10,12 +11,13 @@ from run import OsmCalc
 curr_date = datetime.date.today().strftime("%Y-%m-%d")
 o = OsmCalc()
 city = sys.argv[1]
-filename = "images" + os.sep + "München-Bezirke" + os.sep + city.replace(" ", "_") + "_" + curr_date
+city_name_zerofilled = re.sub(' ([0-9]) ', ' 0\g<1> ', city)
+filename = "images" + os.sep + "München-Bezirke" + os.sep + city_name_zerofilled.replace(" ", "_") + "_" + curr_date
 print(filename)
 G_bike = o.getBike(city)
 G_car = o.getCar(city)
 G = nx.compose(G_car, G_bike)
-o.writeCsv([city + " Bike"]+list(o.calcStats(G_bike, city).values()), "cities_" + curr_date + ".csv")
-o.writeCsv([city + " Car"]+list(o.calcStats(G_car, city).values()), "cities_" + curr_date + ".csv")
+o.writeCsv([city + " Bike"]+list(o.calcStats(G_bike, city).values()), "munich_suburbs_" + curr_date + ".csv")
+o.writeCsv([city + " Car"]+list(o.calcStats(G_car, city).values()), "munich_suburbs_" + curr_date + ".csv")
 o.plotG(G, save=True, filepath=filename+".png", colored=True)
 
